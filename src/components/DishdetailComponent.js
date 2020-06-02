969838b6a664678492d02ab02c921f89194e64da
Bibishan Pandey <bibishan.b.pandey@gmail.com>
2020-06-02 10:22:40 +0545

    comment UI

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,13 +4,15 @@ import { Card, CardImg, CardBody, CardTitle, CardText } from 'reactstrap';
 const RenderDish = ({ dish }) => {
     if (dish != null) {
         return (
-            <Card>
-                <CardImg width="100%" src={dish.image} alt={dish.name} />
-                <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                </CardBody>
-            </Card>
+            <div className={"col-12 col-md-6 mt-3"}>
+                <Card>
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            </div>
         );
     } else {
         return (
@@ -23,27 +25,32 @@ const Comment = ({ comment }) => {
     return (
         <li key={comment.id}>
             <div className={"row"}>
-                <div className={"col-6 align-self-center"}>
-                    <p className={"lead font-weight-bold font-italic font-dark"}>
+                <div className={"col-7 align-self-center"}>
+                    <p className={"font-weight-bold font-italic font-dark"}>
                         {comment.author}
                     </p>
                 </div>
-                <div className={"col-6 align-self-center"}>
+                <div className={"col-5 align-self-center"}>
                     <p className={"d-flex justify-content-end font-italic font-weight-light font-light"}>
                         {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}
                     </p>
                 </div>
             </div>
-            <p className={"pl-3 text-secondary"}>{comment.comment}</p>
+            <div className={"row"}>
+                <div className={"col-12 align-self-center pl-4"}>
+                    <p className={"text-secondary"}><i class="fa fa-comment" aria-hidden="true"></i> {comment.comment}</p>
+                </div>
+            </div>
+            <hr />
         </li>
     );
 };
 
 const RenderComments = ({ comments }) => {
     return (
-        <div>
+        <div className={"col-12 col-md-6 mt-3"}>
             <p className={"display-4 text-dark"}>Comments</p>
-            <hr/>
+            <hr />
             <ul className={"list-unstyled"}>
                 {comments.map(comment => <Comment key={comment.id} comment={comment} />)}
             </ul>
@@ -55,12 +62,8 @@ const DishDetail = (props) => {
     return (
         <div className={"container"}>
             <div className={"row mb-5"}>
-                <div className={"col-12 col-md-6 mt-3"}>
-                    <RenderDish dish={props.dish} />
-                </div>
-                <div className={"col-12 col-md-6 mt-3"}>
-                    <RenderComments comments={props.dish.comments} />
-                </div>
+                <RenderDish dish={props.dish} />
+                <RenderComments comments={props.dish.comments} />
             </div>
         </div>
     );
